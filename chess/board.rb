22eefcf29ea52b49d220244be9d3716f5115ee8d
karenhlai <laikarenh@gmail.com>
2019-03-19 12:21:13 -0700

    Create working Board in Board Class

--- chess/board.rb
+++ chess/board.rb
@@ -1,25 +1,43 @@
 require "byebug"
+require_relative 'piece.rb' 
+
 class Board
 
   attr_reader :grid
   def initialize
-    @grid = Array.new(8) { Array.new(8, "X") } 
+    @grid = Array.new(8) { Array.new(8, NullPiece.instance) } 
+    @grid[0][0] = Rook.new 
+    @grid[0][1] = Knight.new
+    @grid[0][2] = Bishop.new
+    @grid[0][3] = King.new
+    @grid[0][4] = Queen.new
+    @grid[0][5] = Bishop.new
+    @grid[0][6] = Knight.new
+    @grid[0][7] = Rook.new
+    @grid[1] = Array.new(8) { |col| col = Pawn.new }
+
+    @grid[6] = Array.new(8) { |col| col = Pawn.new }
+    @grid[7][0] = Rook.new 
+    @grid[7][1] = Knight.new
+    @grid[7][2] = Bishop.new
+    @grid[7][3] = King.new
+    @grid[7][4] = Queen.new
+    @grid[7][5] = Bishop.new
+    @grid[7][6] = Knight.new
+    @grid[7][7] = Rook.new
   end
 
+
+
+
   def move_piece(start_pos, end_pos)
-    debugger
-    self.grid.each_with_index do |row, row_idx| # an entire array
-      row.each_with_index do |col, col_idx| 
-        piece = col
-        current_pos = self.grid[row_idx, col_idx]
-        if current_pos == start_pos 
-          
-          current_pos = piece
-          self.grid[end_pos] = piece 
-          start_pos = nil
-        end
-      end
-    end
+    old_row, old_col = start_pos[0], start_pos[1]
+    raise "You're trying to move nothing!" if self[[old_row, old_col]] == NullPiece.instance
+    new_row, new_col = end_pos[0], end_pos[1]
+    current_val = self[[old_row, old_col]]
+    raise 'Wrong move, bro!!' if self[[new_row, new_col]] != NullPiece.instance
+    self[[new_row, new_col]] = current_val
+    self[[old_row, old_col]] = NullPiece.instance
   end
 
   def []=(pos, value) # [[1, 2]] = value
--- chess/piece.rb
+++ chess/piece.rb
@@ -1,5 +1,42 @@
+require "singleton"
 class Piece
-  def initialize(pos)
-    @pos = pos
+  def initialize
+
+  end
+end
+
+class Rook < Piece
+  def initialize
+  end
+end
+
+class Pawn < Piece
+  def initialize
+  end
+end
+
+class Queen < Piece
+  def initialize
+  end
+end
+
+class NullPiece < Piece
+  include Singleton
+  def initialize
+  end
+end
+
+class Knight < Piece
+  def initialize
+  end
+end
+
+class Bishop < Piece
+  def initialize
+  end
+end
+
+class King < Piece
+  def initialize
   end
 end
